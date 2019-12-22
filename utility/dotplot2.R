3bff84269af6fdf3f215d68585ea762366ee5cf0
Yusuke Matsui <mail.to.matsui@gmail.com>
2019-12-22 16:12:20 +0900

    updated

---
+++ utility/dotplot2.R
@@ -0,0 +1,25 @@
+dotplot2 = function(x,ntop=10,type="up",font.size=5){
+  dot_df = x
+  dot_df = dot_df[dot_df$p.adjust <= 0.05,]
+  dot_df = dot_df[order(dot_df$p.adjust),]
+  dot_df$type = "upregulated"
+  dot_df$type[dot_df$NES < 0] = "downregulated"
+  dot_df$GeneRatio = sapply(strsplit(dot_df$leading_edge,", "),function(x)x[3])
+  dot_df$GeneRatio = sapply(strsplit(dot_df$GeneRatio,"=|%"),function(x)as.numeric(x[2]))
+  
+  if(type=="up"){
+    df = dot_df[dot_df$type== "upregulated",]
+  }else if(type=="down"){
+    df = dot_df[dot_df$type== "downregulated",]
+  }else{
+    stop("invalid argument for type!")
+  }
+  
+  if(nrow(df) <= ntop){ntop=nrow(df)}
+  ggplot(df[1:ntop,], aes(x = GeneRatio, y = fct_reorder(Description, GeneRatio))) + 
+    geom_point(aes(size = GeneRatio, color = p.adjust)) +
+    theme_bw(base_size = font.size) +
+    scale_colour_gradient(limits=c(0, 0.05), low="red") +
+    ylab(NULL) +
+    ggtitle("GO pathway enrichment")
+}
